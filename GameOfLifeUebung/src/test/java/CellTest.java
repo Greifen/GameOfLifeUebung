083a71752701ab7769629343b1eb745654675f4e
karin <karin@DESKTOP-JFTOVL2>
2025-01-04 23:11:17 +0100

    params so klappt es auch mit den dependencies in maven ohne libary <artifactId>junit-jupiter-params</artifactId>         	<version>5.10.0</version>

--- GameOfLifeUebung/src/test/java/CellTest.java
+++ GameOfLifeUebung/src/test/java/CellTest.java
@@ -1,156 +1,43 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CellTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+    @ParameterizedTest
+    @CsvSource({
+            // Lebende Zelle (ALIVE)
+            "ALIVE, 0, DEAD", // Zu wenig Nachbarn -> stirbt
+            "ALIVE, 1, DEAD", // Zu wenig Nachbarn -> stirbt
+            "ALIVE, 2, ALIVE", // Überlebt
+            "ALIVE, 3, ALIVE", // Überlebt
+            "ALIVE, 4, DEAD", // Zu viele Nachbarn -> stirbt
+            "ALIVE, 5, DEAD", // Zu viele Nachbarn -> stirbt
+            "ALIVE, 6, DEAD", // Zu viele Nachbarn -> stirbt
+            "ALIVE, 7, DEAD", // Zu viele Nachbarn -> stirbt
+            "ALIVE, 8, DEAD", // Zu viele Nachbarn -> stirbt
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+            // Tote Zelle (DEAD)
+            "DEAD, 0, DEAD", // Keine Reproduktion
+            "DEAD, 1, DEAD", // Keine Reproduktion
+            "DEAD, 2, DEAD", // Keine Reproduktion
+            "DEAD, 3, ALIVE", // Reproduktion -> wird lebendig
+            "DEAD, 4, DEAD", // Keine Reproduktion
+            "DEAD, 5, DEAD", // Keine Reproduktion
+            "DEAD, 6, DEAD", // Keine Reproduktion
+            "DEAD, 7, DEAD", // Keine Reproduktion
+            "DEAD, 8, DEAD" // Keine Reproduktion
+    })
+    void testUpdateState(CellState initialState, int neighbors, CellState expectedState) {
+        // Arrange
+        Cell cell = new Cell(initialState);
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+        // Act
+        cell.updateState(neighbors);
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	
-	// Tests für lebende Zellen (ALIVE)
-    @Test
-    void testAlive0Neighbours() {
-		Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(0);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive1Neighbour() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(1);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive2Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(2);
-        assertEquals(CellState.ALIVE, cell.getState());
-    }
-
-    @Test
-    void testAlive3Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(3);
-        assertEquals(CellState.ALIVE, cell.getState());
-    }
-
-    @Test
-    void testAlive4Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(4);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive5Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(5);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive6Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(6);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive7Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(7);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testAlive8Neighbours() {
-        Cell cell = new Cell(CellState.ALIVE);
-        cell.updateState(8);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    // Tests für tote Zellen (DEAD)
-    @Test
-    void testDead0Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(0);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead1Neighbour() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(1);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead2Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(2);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead3Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(3);
-        assertEquals(CellState.ALIVE, cell.getState());
-    }
-
-    @Test
-    void testDead4Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(4);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead5Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(5);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead6Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(6);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead7Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(7);
-        assertEquals(CellState.DEAD, cell.getState());
-    }
-
-    @Test
-    void testDead8Neighbours() {
-        Cell cell = new Cell(CellState.DEAD);
-        cell.updateState(8);
-        assertEquals(CellState.DEAD, cell.getState());
+        // Assert
+        assertEquals(expectedState, cell.getState());
     }
 }
 
