d3f3503c883f3f1e1e5904a43d4cdcaad69496e2
karin <karin@DESKTOP-JFTOVL2>
2025-01-04 22:54:48 +0100

    Cell und Celltests

--- GameOfLifeUebung/src/main/java/Cell.java
+++ GameOfLifeUebung/src/main/java/Cell.java
@@ -1,4 +1,28 @@
 
 public class Cell {
 
+	private CellState cellstate;
+
+	public Cell(CellState cellstate) {
+		this.cellstate = cellstate;
+	}
+
+	public void updateState(int neighbours) {
+		if (cellstate == CellState.ALIVE) {
+			//underpopulation and overcrowding
+            if (neighbours < 2 || neighbours > 3) {
+                cellstate = CellState.DEAD;
+            }
+            //survival
+        } else {
+        	//reproduction
+            if (neighbours == 3) {
+                cellstate = CellState.ALIVE;
+            }
+        }
+	}
+
+	public CellState getState() {
+		return cellstate;
+	}
 }
--- GameOfLifeUebung/src/test/java/CellTest.java
+++ GameOfLifeUebung/src/test/java/CellTest.java
@@ -24,9 +24,133 @@ class CellTest {
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	
+	// Tests für lebende Zellen (ALIVE)
+    @Test
+    void testAlive0Neighbours() {
+		Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(0);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
 
+    @Test
+    void testAlive1Neighbour() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(1);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testAlive2Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(2);
+        assertEquals(CellState.ALIVE, cell.getState());
+    }
+
+    @Test
+    void testAlive3Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(3);
+        assertEquals(CellState.ALIVE, cell.getState());
+    }
+
+    @Test
+    void testAlive4Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(4);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testAlive5Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(5);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testAlive6Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(6);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testAlive7Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(7);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testAlive8Neighbours() {
+        Cell cell = new Cell(CellState.ALIVE);
+        cell.updateState(8);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    // Tests für tote Zellen (DEAD)
+    @Test
+    void testDead0Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(0);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead1Neighbour() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(1);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead2Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(2);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead3Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(3);
+        assertEquals(CellState.ALIVE, cell.getState());
+    }
+
+    @Test
+    void testDead4Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(4);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead5Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(5);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead6Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(6);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead7Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(7);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
+
+    @Test
+    void testDead8Neighbours() {
+        Cell cell = new Cell(CellState.DEAD);
+        cell.updateState(8);
+        assertEquals(CellState.DEAD, cell.getState());
+    }
 }
+
