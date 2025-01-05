433e177e08f323d3f8bf780c9dacf851833ed7db
karin <karin@DESKTOP-JFTOVL2>
2025-01-05 19:17:05 +0100

    Cell

--- GameOfLifeUebung/src/main/java/Cell.java
+++ GameOfLifeUebung/src/main/java/Cell.java
@@ -1,4 +1,24 @@
 
 public class Cell {
 
+	private CellState cellstate;
+
+	public Cell(CellState cellstate) {
+		this.cellstate = cellstate;
+	}
+
+	public void nextState(int neighbour) {
+		if(cellstate==CellState.ALIVE && (neighbour<2 ||neighbour>3))
+		{
+			cellstate=CellState.DEAD;
+		}
+		else if(cellstate==cellstate.DEAD && neighbour==3) {
+			cellstate=CellState.ALIVE;
+		}
+	}
+
+	public CellState getState() {
+		return cellstate;
+	}
+
 }
--- GameOfLifeUebung/src/test/java/CellTest.java
+++ GameOfLifeUebung/src/test/java/CellTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CellTest {
 
@@ -24,9 +26,31 @@ class CellTest {
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@CsvSource({
+		"ALIVE,0,DEAD",
+		"ALIVE,1,DEAD",
+		"ALIVE,2,ALIVE",
+		"ALIVE,3,ALIVE",
+		"ALIVE,4,DEAD",
+		"ALIVE,5,DEAD",
+		"ALIVE,6,DEAD",
+		"ALIVE,7,DEAD",
+		"ALIVE,8,DEAD",
+		"DEAD,0,DEAD",
+		"DEAD,1,DEAD",
+		"DEAD,2,DEAD",
+		"DEAD,3,ALIVE",
+		"DEAD,4,DEAD",
+		"DEAD,5,DEAD",
+		"DEAD,6,DEAD",
+		"DEAD,7,DEAD",
+		"DEAD,8,DEAD"
+		})
+	void testAlive0Neighbours(CellState initalState, int neighbour, CellState expectedState) {
+		Cell cell = new Cell(initalState);
+		cell.nextState(neighbour);
+		assertEquals(expectedState,cell.getState());
 	}
 
 }
